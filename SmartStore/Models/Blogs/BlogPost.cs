c516f40ee72cba40bf146d463d1eaccbd9fe8f3f
Onke Tshaka <onktshaka200@gmail.com>
2022-06-18 21:48:23 +0200

    migrations

---
+++ SmartStore/Models/Blogs/BlogPost.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartStore.Models.Blogs
+{
+    public class BlogPost
+    {
+
+        [Key, ScaffoldColumn(false), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public string description { get; set; }
+
+        public string body { get; set; }
+
+        //public IList<Tag> Tags { get; set; }
+
+        public DateTime createdOn { get; set; } = DateTime.Now;
+
+        public string image { get; set; }
+
+        public bool AllowComments { get; set; }
+    }
+}
---
+++ SmartStore/Models/Blogs/Tag.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SmartStore.Models.Blogs
+{
+    public class Tag
+    {
+
+        [Key, ScaffoldColumn(false), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string Name { get; set; }
+
+        public string SeName { get; set; }
+
+        public int BlogPostCount { get; set; }
+    }
+}
